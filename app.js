4c727cf984f5ae760fb4d62669fbe3dab2eacb30
luisk06 <luismorillovergel@gmail.com>
2016-11-24 14:46:18 -0500

    Final Product

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 (function(){
 'use strict';
-  angular.module('mod1App',[])
+  angular.module('LunchCheck',[])
 
-  .controller('foodController',foodController);
+  .controller('LunchCheckController',LunchCheckController);
 
-    foodController.$inject =['$scope'];
-    function foodController($scope){
+    LunchCheckController.$inject =['$scope'];
+    function LunchCheckController($scope){
+      $scope.foodList = "";
       $scope.calculateAmount = function(){
-        console.log("it's going well");
+
+        var food = $scope.foodList;
+        var foodSplit = food.split(","); //Create array with comma-separated element
+        var FoodSplitValid = foodSplit.filter(String); //Taking out empty elements.. Ex [cucumber, bread ,, turkey]
+        var totalFood = FoodSplitValid.length;
+
+        if (totalFood >= 1){
+              $scope.messageColor = {
+                            "color" : "green",
+                        }
+              if (totalFood<=3) {
+                $scope.messageFood = "Enjoy!";
+              } else {
+                $scope.messageFood = "Too Much!";
+              }
+          }else {
+              $scope.messageColor = {
+                        "color" : "red",
+                    }
+              $scope.messageFood ="Please enter data first";
+          }
+
       }
     }
 
